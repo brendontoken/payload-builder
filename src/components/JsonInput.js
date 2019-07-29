f49e80cdaad8a18e901e0caaa5ec2be4b9d07cac
Brendon Duncan <brendon.duncan@tokenized.com>
2019-07-29 15:56:21 +1200

    Resetting the input field.

--- src/components/JsonInput.js
+++ src/components/JsonInput.js
@@ -1,25 +1,47 @@
 import React, { Component } from 'react'
 
 export class JsonInput extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: '',
+      isValid: true
+    }
 
-  onInputChanged(e) {
-    console.log('onInputChanged');
   }
 
-  onResetInput(e) {
+  handleInputChange = (e) => {
+    const text = e.target.value;
+    this.setState({ inputValue: text });
+    console.log(`onInputChanged "${text}"`);
+    let parsed;
+    try {
+      parsed = JSON.parse(text);
+      this.setState({ isValid: true });
+    } catch(e) {
+      this.setState({ isValid: false });
+      return;
+    }
+  }
+
+  handleReset = (e) => {
     e.preventDefault();
     console.log('resetInput');
+    this.setState({ inputValue: '' });
   }
-
 
   render() {
     return (
       <div>
         <div>
-          <input onInput={this.onInputChanged}></input>
+          <input 
+            onChange={this.handleInputChange}
+            value={this.state.inputValue} 
+            style={this.state.isValid ? { } : { border: 'solid 2px red'}}>
+          </input>
         </div>
         <div>
-          <button onClick={this.onResetInput}>
+          <button onClick={this.handleReset}>
             Reset
           </button>
         </div>
