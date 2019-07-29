dc8a26fb3fab296c771078582174a6ab85d221c0
Brendon Duncan <brendon.duncan@tokenized.com>
2019-07-29 16:24:31 +1200

    Updates

---
+++ precache-manifest.d8475c7cb61b114f52a3fed8e27b5412.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5d6d31260e8e8430e9c0154483634e2",
+    "url": "/payload-builder/index.html"
+  },
+  {
+    "revision": "a0e8cd85d32ba5bb5566",
+    "url": "/payload-builder/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/payload-builder/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "a0e8cd85d32ba5bb5566",
+    "url": "/payload-builder/static/js/main.71b5b806.chunk.js"
+  },
+  {
+    "revision": "e3e341c42896fc41aaad",
+    "url": "/payload-builder/static/js/runtime~main.215163c0.js"
+  }
+]);
