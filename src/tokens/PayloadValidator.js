d6e070fd3d189088e9d8d143ec90b6231900f0e8
Brendon Duncan <brendon.duncan@tokenized.com>
2019-08-11 18:00:06 +1200

    Validating JSON using generated actions.

--- src/tokens/PayloadValidator.js
+++ src/tokens/PayloadValidator.js
@@ -1,74 +1,20 @@
-import { ContractOffer, OpReturnMessage, Permission, Timestamp, Entity, PublicKeyHash } from 'tokenized';
-import BallotCastValidator from './actions/BallotCastValidator';
-import BallotCountedValidator from './actions/BallotCountedValidator';
-import ContractAddressChangeValidator from './actions/ContractAddressChangeValidator';
-import EstablishmentValidator from './actions/EstablishmentValidator';
-import ThawValidator from './actions/ThawValidator';
-import VoteValidator from './actions/VoteValidator';
+import { ContractOffer, OpReturnMessage, Permission, Timestamp, Entity, PublicKeyHash, Vote, Establishment } from 'tokenized';
 import { objectIfPresent, stringIfPresent } from './primitiveValidators';
 
 class PayloadValidator {
 
   _addActionValidator = (actionValidator) => {
-    this.actionValidators[actionValidator.actionCode] = actionValidator
+    this.actionValidators[actionValidator.Type()] = actionValidator
   }
 
   _assembleActionValidators = () => {
     this.actionValidators = {};
-    this._addActionValidator(new BallotCastValidator());
-    this._addActionValidator(new BallotCountedValidator());
-    this._addActionValidator(new ContractAddressChangeValidator());
-    this._addActionValidator(new EstablishmentValidator());
-    this._addActionValidator(new ThawValidator());
-    this._addActionValidator(new VoteValidator());
+    this._addActionValidator(new Establishment());
+    this._addActionValidator(new Vote());
   }
 
   constructor() {
     this._assembleActionValidators();
-  }
-
-  _testPackage = () => {
-    console.log('_testPackage()');
-    let voteSystemsAllowed = [ true, true ];
-
-    let permission = new Permission();
-    permission.permitted = true;
-    permission.administration_proposal = true;
-    permission.holder_proposal = false;
-    permission.voting_systems_allowed = voteSystemsAllowed;
-
-    // Note: Permissions can be different for each field.
-	  let permissions = [...Array(20)].map(_ => permission);
-
-    // Serialize auth flags
-    let authFlags = Permission.WriteAuthFlags(permissions);
-
-    // Generate a new contract offer action
-    let action = new ContractOffer();
-    action.contract_name = 'Test';
-    action.body_of_agreement_type = 2;
-    action.body_of_agreement = [...Buffer.from('<contract agreement>', 'ascii')];
-    action.contract_type = 'Test Type';
-    action.contract_auth_flags = authFlags;
-    action.supporting_docs = [];
-    action.contract_expiration = new Timestamp();
-    action.issuer = new Entity();
-    action.voting_systems = [];
-    action.oracles = [];
-    action.master_pkh = new PublicKeyHash();
-    // Specify any other fields necessary
-    // ...
-
-    // Serialize action
-    let isTest = true; // use "test.tokenized" OP_RETURN signature
-    let actionData = OpReturnMessage.Serialize(action, isTest);
-
-    // Convert to hex
-    console.log("Contract Offer Hex :\n", actionData);
-
-    // Output as human readable string
-    console.log("Contract Offer text :\n", JSON.stringify(action));
-
   }
 
   _validateHeader = (header) => {
@@ -103,9 +49,12 @@ class PayloadValidator {
     const actionContents = objectIfPresent(spec, 'top level', 'actionContents');
     if (!actionContents) { throw new Error('"actionContents" missing.'); }
 
-    return actionValidator.validate(actionContents, version);
+    const validationErrorMessage = actionValidator.parse(JSON.stringify(actionContents));
+    if (validationErrorMessage) {
+      throw new Error(`Validation failed. ${validationErrorMessage}`);
+    }
 
-    //this._testPackage();
+    return actionValidator;
   }
 
 }
